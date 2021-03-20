7e935e4d6403a3b6c09052d298de36bcd2f87b7b
sachniwe <44708156+sachniwe@users.noreply.github.com>
2021-03-20 12:26:23 +0200

    Added initial backend components

---
+++ ee-backend/src/main/java/ee/eestienrgia/sales/eebackend/EeBackendApplication.java
@@ -0,0 +1,13 @@
+package ee.eestienrgia.sales.eebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EeBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EeBackendApplication.class, args);
+	}
+
+}
---
+++ ee-backend/src/test/java/ee/eestienrgia/sales/eebackend/EeBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package ee.eestienrgia.sales.eebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EeBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
